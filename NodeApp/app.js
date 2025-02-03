957c1c0b94a14852449195cc65333b01f86071b7
Ayman Osman <95719911+AymanAhmedOsman@users.noreply.github.com>
2025-02-03 09:42:14 +0200

    Update app.js

--- NodeApp/app.js
+++ NodeApp/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
-    res.send('Hello Ahmed from Rabbit SAP GUI 1 & GUI 2')
+    res.send('Hello IntegraTechnologie')
   })
   
   app.listen(port, () => {
